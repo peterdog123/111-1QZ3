2c2fa747a5595f5548587995788c8d75501e6153
睿翔 唐 <109111119@mail.aeust.edu.tw>
2022-11-14 23:58:11 +0800

    6666

---
+++ 111-1QZ3/WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string[] s_City = new string[3] { "台北市", "新北市", "台中市" };
+        string[,] s_Area = new string[3, 3] {
+            {"中正區", "文山區", "大安區"},
+            {"淡水區", "石碇區", "土城區"},
+            {"西屯區", "北屯區", "東區"}
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                for(int i_Ct = 0; i_Ct< s_City.Lenght; i_Ct++)
+                {
+                    ListItem o_L = new ListItem();
+                    o_L.Text = o_L.Value =s_City[i_Ct];
+                    dpl_City.Items.Add(o_L);
+                }
+            }
+            mt_GenSecondList();
+        }
+
+        protected System.Void dpl_City_SelectedIndexChanged(System.Object sender, System.EventArgs e)
+        {
+            mt_GenSecondList();
+        }
+        protected void mt_GenSecondList()
+        {
+            int i_index = dpl_City.SelectedIndex;
+            dpl_Area.Items.Clear();
+            for(int i_Ct = 0; i_Ct < s_Area.GetLenght(1); i_Ct++)
+            {
+                ListItem o_L = new ListItem();
+                o_L.Text = o_L.Value = s_Area[i_index,i_Ct];
+                dpl_Area.Items.Add(o_L);
+            }
+        }
+
+        protected System.Void rbl_Phone_SelectedIndexChanged(System.Object sender, System.EventArgs e)
+        {
+            if(rbl_Phone.SelectedIndex == 2)
+            {
+                txt_Phone.Visible = false;
+            }
+            else
+            {
+                txt_Phone.Visible = true;
+            }
+        }
+    }
+}
---
+++ 111-1QZ3/WebForm2.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ3
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lb_Msg.Text =
+                "保單號碼: &nbsp;" + Request.Form.Get("tb_Num") + "<br />" +
+                "通訊種類: &nbsp;" + Request.Form.Get("rbl_Phone") + "<br />";
+
+            if (!Request.Form.Get("rbl_Phone").Equals("無"))
+            {
+                lb_Msg.Text += "通訊種類: &nbsp;" + Request.Form.Get("txt_Phone") + "<br />";
+            }
+            lb_Msg.Text +=
+                "所在城市: &nbsp;" + Request.Form.Get("dpl_City") + "<br />" +
+                "通訊種類: &nbsp;" + Request.Form.Get("dpl_Area") + "<br />";
+        }
+    }
+}
